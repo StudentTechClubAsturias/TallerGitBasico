d5686f118c2beeeb1037e3792bf73ba1bdee0aea
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2015-11-22 17:10:17 +0100

    Esqueleto método

--- esPrimo.py
+++ esPrimo.py
@@ -5,28 +5,31 @@ Autor: X
 
 #Nota: los primos son: Naturales, > 1, divisibles entre ellos mismo y entre 1
 def esPrimo(n):
-	"""
-	Metodo para comprobar si un numero n dado es primo o no
-	params: un numero n
-	return: True si es primo y False de no serlo
-	"""
+    """
+    Metodo para comprobar si un numero n dado es primo o no
+    params: un numero n
+    return: True si es primo y False de no serlo
+    """
 	
-	#Si es menor que 2 no puede ser primo
-	if n < 2:	return False
+    #Si es menor que 2 no puede ser primo
+    if n < 2:	return False
 	
-	#El rango de 2 hasta n
-	for i in range(2, n):
-		if n / i == 1:	return False
+    #El rango de 2 hasta n
+    for i in range(2, n):
+        if n / i == 1:	return False
 	
-	return True
+    return True
 
 def esPrimoIntervalo(i, k):
-	"""
-	Metodo que comprueba si los numeros de un intervalo son primos o no
-	params: intervalo [i ,k)
-	return: retorna una lista de duplas
-	"""
-	return [(x, esPrimo(x)) for x in range(i, k)]
+    """
+    Metodo que comprueba si los numeros de un intervalo son primos o no
+    params: intervalo [i ,k)
+    return: retorna una lista de duplas
+    """
+    return [(x, esPrimo(x)) for x in range(i, k)]
+
+def cuantosDivisores(n):
+    # Devolver la cantidad de divisores que tiene el número, incluyendo 1 y a sí mismo
 
 
 """
