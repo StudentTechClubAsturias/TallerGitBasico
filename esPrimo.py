09ff4007c307272ff82d192e5258658c58444c1e
Esteban Montes <sankoxh@live.com>
2015-11-22 16:23:06 +0100

    Ejemplo: Se ha empezado a escribir el script para detectar numeros primos

---
+++ esPrimo.py
@@ -0,0 +1,14 @@
+"""
+Autor: X
+"""
+
+
+#Nota: los primos son: Naturales, > 1, divisibles entre ellos mismo y entre 1
+def esPrimo(n):
+	"""
+	Metodo para comprobar si un numero n dado es primo o no
+	"""
+	
+	if n < 2:	return False
+	
+	
