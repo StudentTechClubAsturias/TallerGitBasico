a0de5b1fb12439c2a4088a27762aa7dc17b4353e
Esteban Montes <sankoxh@live.com>
2015-11-22 16:30:16 +0100

    Ejemplo: Actualizado el script para detectar numeros primos, ahora es funcional

--- esPrimo.py
+++ esPrimo.py
@@ -9,6 +9,15 @@ def esPrimo(n):
 	Metodo para comprobar si un numero n dado es primo o no
 	"""
 	
+	#Si es menor que 2 no puede ser primo
 	if n < 2:	return False
 	
+	#El rango de 2 hasta n
+	for i in range(2, n):
+		if n % i == 0:	return False
 	
+	return True
+	
+
+primos = [3, 5, 7, 11, 13, 17, 19, 23, 29]
+print [(x, esPrimo(x)) for x in primos]
